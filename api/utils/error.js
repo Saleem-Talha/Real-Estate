daa416fb85ee0605cde578a9486e2acb48c861ef
Saleem Talha <saleemtalha967@gmail.com>
2024-05-16 08:59:36 -0700

    Created the sign in api route and function

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,8 +1,10 @@
 //to create a manual error like password is too short
 
-export const errorHandler = (statusCode, message) => {
+const errorHandler = (statusCode, message) => {
     const error = new Error();
     error.statusCode = statusCode;
     error.message = message;
     return error;
 }
+
+export default errorHandler;
