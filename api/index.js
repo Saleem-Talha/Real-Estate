6f3acadd29c5f6baeca29827ab7d83a6534a265d
Saleem Talha <saleemtalha967@gmail.com>
2024-05-21 06:26:05 -0700

    Created user update api

--- api/index.js
+++ api/index.js
@@ -1,32 +1,38 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-const app = express();
-app.use(express.json());
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connect to MongoDB')
-}).catch((err)=>{
-    console.log(err)
-})
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
 
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-//middleware error handler
-app.use((err,req,res,next)=>{
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message,
-    });
+// Middleware error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
- 
