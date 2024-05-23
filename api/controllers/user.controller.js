80d83c5c30b77b48d8f65f7c283f173682078545
Saleem Talha <saleemtalha967@gmail.com>
2024-05-23 09:07:22 -0700

    Created user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Listing from "../models/listing.models.js"
 import errorHandler from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
 
@@ -44,4 +45,16 @@ export const deleteUser = async(req,res,next) =>{
 } catch(error){
   next(error)
 }
+}
+
+
+export const getuserListing = async(req,res,next) =>{
+  try{
+    if(req.user.id === req.params.id){
+       const listings = await Listing.find({ userRef: req.params.id});
+       res.status(200).json(listings);
+      }  
+  }catch(error){
+    return next(errorHandler(401,'You can only view your own listings'));
+  }
 }
