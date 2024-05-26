bc8182f5c4ff07d6e1c2f5a9e04f6369d475117f
Saleem Talha <saleemtalha967@gmail.com>
2024-05-26 07:38:21 -0700

    Created search functionality - api route,controller function , ui, and listItem component

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -38,63 +38,58 @@ export const updateListing = async (req, res, next) => {
   }
 
   try {
-    const updateListing = await Listing.findByIdAndUpdate(req.params.id,req.body,{new:true});
+    const updateListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
     res.status(200).json(updateListing);
   } catch (e) {
     next(e);
   }
 };
 
-
-export const getListing = async(req, res, next) =>{
-  try{
+export const getListing = async (req, res, next) => {
+  try {
     const listing = await Listing.findById(req.params.id);
-    if(!listing){
-      return next(errorHandler(404,'Listing not found'));
-
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
     }
     res.status(200).json(listing);
-  }catch(e){
+  } catch (e) {
     next(e);
   }
-}
+};
 
 export const getListings = async (req, res, next) => {
   try {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
-
-    if (offer === undefined || offer === 'false') {
+    if (offer === undefined || offer === "false") {
       offer = { $in: [false, true] };
     }
 
     let furnished = req.query.furnished;
-
-    if (furnished === undefined || furnished === 'false') {
+    if (furnished === undefined || furnished === "false") {
       furnished = { $in: [false, true] };
     }
 
     let parking = req.query.parking;
-
-    if (parking === undefined || parking === 'false') {
+    if (parking === undefined || parking === "false") {
       parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
-
-    if (type === undefined || type === 'all') {
-      type = { $in: ['sale', 'rent'] };
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
     }
 
-    const searchTerm = req.query.searchTerm || '';
-
-    const sort = req.query.sort || 'createdAt';
-
-    const order = req.query.order || 'desc';
-
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
     const listings = await Listing.find({
-      name: { $regex: searchTerm, $options: 'i' },
+      name: { $regex: searchTerm, $options: "i" }, //$options: "i"  to remove case sensivity when seraching
       offer,
       furnished,
       parking,
@@ -103,7 +98,6 @@ export const getListings = async (req, res, next) => {
       .sort({ [sort]: order })
       .limit(limit)
       .skip(startIndex);
-
     return res.status(200).json(listings);
   } catch (error) {
     next(error);
